fbe7fac045ba1903fe04b24eb3af9fb4c23408de
Jasper <limbagojasper@gmail.com>
2023-04-11 10:33:48 +0300

    Added avatar scaling

--- Assets/Scripts/HandPoseAnimator.cs
+++ Assets/Scripts/HandPoseAnimator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using RootMotion.FinalIK;
 
 public class HandPoseAnimator : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class HandPoseAnimator : MonoBehaviour
     public ActionBasedController leftController;
     public ActionBasedController rightController;
     public Animator animator;
+    public VRIK ik;
+
+    public bool isScaled = false;
 
     private void Awake()
     {
@@ -26,6 +30,20 @@ public class HandPoseAnimator : MonoBehaviour
         animator.SetFloat("Trigger_Right", rightController.activateAction.action.ReadValue<float>());
         animator.SetFloat("Grip_Right", rightController.selectAction.action.ReadValue<float>());
 
+        //Scale the avatar to user's height
+        if(leftController.activateAction.action.ReadValue<float>() > 0.5 && isScaled == false)
+        {
+            ScaleAvatar();
+            isScaled = true;
+        }
+
+    }
+
+    void ScaleAvatar()
+    {
+        float sizeF = (ik.solver.spine.headTarget.position.y - ik.references.root.position.y) / (ik.references.head.position.y - ik.references.root.position.y);
+        ik.references.root.localScale *= sizeF;
+        Debug.Log("Scaling avatar");
     }
 
     //animator.SetFloat("Trigger_" + whichHand, controller.activateAction.action.ReadValue<float>());
