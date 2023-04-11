9a1b8c5c18264ca3990a0b53fe5bd0606126dfc3
Jasper <limbagojasper@gmail.com>
2023-04-11 09:14:42 +0300

    Fix HandPose Animator
    Get reference to controllers at runtime

--- Assets/Scripts/HandPoseAnimator.cs
+++ Assets/Scripts/HandPoseAnimator.cs
@@ -6,13 +6,28 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class HandPoseAnimator : MonoBehaviour
 {
 
-    public ActionBasedController controller;
+    public ActionBasedController leftController;
+    public ActionBasedController rightController;
     public Animator animator;
-    public string whichHand = "";
+
+    private void Awake()
+    {
+        leftController = GameObject.Find("LeftHand Controller").GetComponent<ActionBasedController>();
+        rightController = GameObject.Find("RightHand Controller").GetComponent<ActionBasedController>();
+    }
 
     void Update()
     {
-        animator.SetFloat("Trigger_" + whichHand, controller.activateAction.action.ReadValue<float>());
-        animator.SetFloat("Grip_" + whichHand, controller.selectAction.action.ReadValue<float>());
+        //Set leftHand Animator Parameters
+        animator.SetFloat("Trigger_Left", leftController.activateAction.action.ReadValue<float>());
+        animator.SetFloat("Grip_Left", leftController.selectAction.action.ReadValue<float>());
+
+        //Set rightHand Animator Parameters
+        animator.SetFloat("Trigger_Right", rightController.activateAction.action.ReadValue<float>());
+        animator.SetFloat("Grip_Right", rightController.selectAction.action.ReadValue<float>());
+
     }
+
+    //animator.SetFloat("Trigger_" + whichHand, controller.activateAction.action.ReadValue<float>());
+    //animator.SetFloat("Grip_" + whichHand, controller.selectAction.action.ReadValue<float>());
 }
