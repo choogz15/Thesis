fca9742254ec80df3dc68811824a5aaecbd9da0d
Jasper <limbagojasper@gmail.com>
2023-05-17 13:40:33 +0300

    Working networked hand animation

--- Assets/Scripts/TestSync.cs
+++ Assets/Scripts/TestSync.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using Normal.Realtime;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
+using RootMotion.FinalIK;
+using UnityEngine.XR.Interaction.Toolkit;
+using System;
 
 public class TestSync : RealtimeComponent<TestModel>
 {
@@ -12,11 +16,87 @@ public class TestSync : RealtimeComponent<TestModel>
 
     public Button callButton;
 
+    public ActionBasedController leftController;
+    public ActionBasedController rightController;
+    public Animator animator;
+
     private void Awake()
     {
         voice = GetComponentInChildren<RealtimeAvatarVoice>();
         voiceChatDictionary = GameObject.Find("VoiceChatDictionary").GetComponent<VoiceChatDictionary>();
 
+        leftController = GameObject.Find("LeftHand Controller").GetComponent<ActionBasedController>();
+        rightController = GameObject.Find("RightHand Controller").GetComponent<ActionBasedController>();
+    }
+
+    protected override void OnRealtimeModelReplaced(TestModel previousModel, TestModel currentModel)
+    {
+        if (previousModel != null)
+        {
+            previousModel.triggerLeftDidChange -= TriggerLeftDidChange;
+            previousModel.gripLeftDidChange -= GripLeftDidChange;
+            previousModel.triggerRightDidChange -= TriggerRightDidChange;
+            previousModel.gripRightDidChange -= GripRightDidChange;
+        }
+
+        if (currentModel != null)
+        {
+            currentModel.triggerLeftDidChange += TriggerLeftDidChange;
+            currentModel.gripLeftDidChange += GripLeftDidChange;
+            currentModel.triggerRightDidChange += TriggerRightDidChange;
+            currentModel.gripRightDidChange += GripRightDidChange;
+        }
+    }
+
+    private void GripRightDidChange(TestModel model, float value)
+    {
+        UpdateRightGrip();
+    }
+
+    private void TriggerRightDidChange(TestModel model, float value)
+    {
+        UpdateRightTrigger();
+    }
+
+    private void GripLeftDidChange(TestModel model, float value)
+    {
+        UpdateLeftGrip();
+    }
+
+    private void TriggerLeftDidChange(TestModel model, float value)
+    {
+        UpdateLeftTrigger();
+    }
+
+    private void Update()
+    {
+        if (isOwnedLocallyInHierarchy)
+        {
+            model.triggerLeft = leftController.activateAction.action.ReadValue<float>();
+            model.gripLeft = leftController.selectAction.action.ReadValue<float>();
+            model.triggerRight = rightController.activateAction.action.ReadValue<float>();
+            model.gripRight = rightController.selectAction.action.ReadValue<float>();
+        }
+    }
+
+    private void UpdateLeftTrigger()
+    {
+        animator.SetFloat("Trigger_Left", model.triggerLeft);
+    }
+
+    private void UpdateLeftGrip()
+    {
+        animator.SetFloat("Grip_Left", model.gripLeft);
+    }
+
+    private void UpdateRightTrigger()
+    {
+        animator.SetFloat("Trigger_Right", model.triggerRight);
+    }
+
+    private void UpdateRightGrip()
+    {
+        animator.SetFloat("Grip_Right", model.gripRight);
     }
 
 }
