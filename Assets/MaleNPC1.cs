5f7ceb13aaf6a55568587410032b83ffc5983a14
Jasper Limbago <limbagojasper@gmail.com>
2023-05-29 22:48:31 +0300

    Cleanup unused rooms

--- Assets/MaleNPC1.cs
+++ Assets/MaleNPC1.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Normal.Realtime;
+using System;
 
 public class MaleNPC1 : MonoBehaviour
 {
@@ -9,9 +11,19 @@ public class MaleNPC1 : MonoBehaviour
     private bool canSpeak = true;
     public float speechInterval = 10f;
 
+    private List<Transform> avatarTransforms;
+    public RealtimeAvatarManager realtimeAvatarManager;
+
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+
+        realtimeAvatarManager.avatarCreated += AvatarCreated;
+    }
+
+    private void AvatarCreated(RealtimeAvatarManager avatarManager, RealtimeAvatar avatar, bool isLocalAvatar)
+    {
+        avatarTransforms.Add(avatar.GetComponent<Transform>());    
     }
 
     private void EnableSpeak()
@@ -19,16 +31,33 @@ public class MaleNPC1 : MonoBehaviour
         canSpeak = true;
     }
 
-
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
         if (!canSpeak) return;
 
-        Debug.Log("On Trigger Enter");
-        audioSource.Play();
-        canSpeak = false;
-        Invoke("EnableSpeak", speechInterval);
+        foreach(var player in avatarTransforms)
+        {
+            if(Vector3.Distance(player.transform.position, transform.position) < 1)
+            {
+                audioSource.Play();
+                canSpeak = false;
+                Invoke("EnableSpeak", speechInterval);
 
-        transform.LookAt(new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), Vector3.up);
+                transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), Vector3.up);
+            }
+        }
     }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (!canSpeak) return;
+
+    //    Debug.Log("On Trigger Enter");
+    //    audioSource.Play();
+    //    canSpeak = false;
+    //    Invoke("EnableSpeak", speechInterval);
+
+    //    transform.LookAt(new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), Vector3.up);
+    //}
 }
